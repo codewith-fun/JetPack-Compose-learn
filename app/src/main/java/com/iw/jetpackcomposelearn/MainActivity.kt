5f9cf73bb80b9fe5c4d1acf41b02406185a549cc
Laxmi kant <laxmipalkant01@gmail.com>
2022-12-13 12:20:51 +0530

    git tag v0.1 -a -m "Release v0.1" git push --follow-tags

--- app/src/main/java/com/iw/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/iw/jetpackcomposelearn/MainActivity.kt
@@ -3,6 +3,9 @@ package com.iw.jetpackcomposelearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,10 +14,12 @@ import com.iw.jetpackcomposelearn.screens.MyRecyclerView
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            myUi()
+            val windowSize = calculateWindowSizeClass(this)
+            myUi(windowSize)
         }
     }
 
@@ -22,12 +27,12 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun myUi(){
+fun myUi(windowSize: WindowSizeClass) {
     val  msg = remember{
         MyRepository().getAllMessage()
     }
     val myrepo = MyRepository()
     val repoMsg = myrepo.getAllMessage()
-    MyRecyclerView(message = msg)
+    MyRecyclerView(message = msg,windowSize)
 }
 
--- app/src/main/java/com/iw/jetpackcomposelearn/screens/CScreens.kt
+++ app/src/main/java/com/iw/jetpackcomposelearn/screens/CScreens.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -23,43 +24,65 @@ import com.iw.jetpackcomposelearn.data.Message
  * initial card
  */
 @Preview(showBackground = true)
-    @Composable
-    fun itemsCard(m : Message){
-        Card(elevation = 5.dp,
-            modifier = Modifier
-                .padding(5.dp)
-        ) {
-            Row(modifier = Modifier.padding(all = 8.dp)) {
-                Image(painter = painterResource(id = m.ic_image),
-                    contentDescription ="Content",
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape).align(CenterVertically)
+@Composable
+fun ReplyAppPreview(m: Message, windowSize: WindowSizeClass){
+        ReleyApp( m, windowSize)
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppPreviewTablet(m: Message, windowSize: WindowSizeClass){
+    ReleyApp(m, windowSize)
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppPreviewDesktop(m: Message, windowSize: WindowSizeClass){
+    ReleyApp(m,  windowSize)
+}
+// release check
+/**
+ * git tag v0.1 -a -m "Release v0.1"
+   git push --follow-tags
+ */
+@Composable
+fun ReleyApp(m:Message,windowSize: WindowSizeClass){
+    Card(elevation = 5.dp,
+        modifier = Modifier
+            .padding(5.dp),
+    ) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(painter = painterResource(id = m.ic_image),
+                contentDescription ="Content",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .align(CenterVertically)
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Column(modifier = Modifier.padding(10.dp,0.dp,0.dp,0.dp)) {
+                Text(
+                    text = "${m?.msg}",
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp,
                 )
                 Spacer(modifier = Modifier.height(5.dp))
-                Column(modifier = Modifier.padding(10.dp,0.dp,0.dp,0.dp)) {
-                    Text(
-                        text = "${m?.msg}",
-                        color = Color.Black,
-                        textAlign = TextAlign.Center,
-                        fontSize = 14.sp,
-                    )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Text(text = "${m?.description}")
-                }
+                Text(text = "${m?.description}")
             }
         }
     }
+}
 
 /**
  * initial recyclerview card
  */
 
 @Composable
-fun MyRecyclerView(message: List<Message>){
+fun MyRecyclerView(message: List<Message>, windowSize: WindowSizeClass){
     LazyColumn{
         items(message){
-            itemsCard(m = it)
+            ReleyApp(it,windowSize)
         }
     }
 }
